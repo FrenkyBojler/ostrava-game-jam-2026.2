cbb087f748b3a2b4b02dcec852745c52f84b21f4
Tomas Polak <tomas@laposa.ie>
2026-09-25 22:49:05 +0200

    add items pickup & drop interaction

--- items/item.gd
+++ items/item.gd
@@ -11,6 +11,8 @@ var item_resource: ItemResource
 
 var image : Sprite2D
 
+var colliders: Array[CollisionShape2D] = []
+
 func _ready() -> void:
 	image = _find_image()
 	assert(image != null, "Missing image")
@@ -22,11 +24,14 @@ func _add_collision_shapes() -> void:
 	var coords := FlagsGridUtils.get_set_coords(item_resource.colliders, 3)
 	for coord: Vector2 in coords:
 		var collision_shape_2d := CollisionShape2D.new()
+		set_collision_layer_value(2, true)
+		set_collision_mask_value(2, true)
 		collision_shape_2d.name = "collider_" + str(coord)
 		collision_shape_2d.shape = collision_shape
+		
 		add_child(collision_shape_2d)
 		collision_shape_2d.position = Vector2(coord.x * CELL_SIZE - CELL_SIZE, coord.y * CELL_SIZE - CELL_SIZE)
-
+		colliders.push_back(collision_shape_2d)
 
 func _find_image() -> Sprite2D:
 	for child in get_children():
@@ -40,3 +45,15 @@ func _add_outline_shader() -> void:
 	
 func toggle_outline(value: bool) -> void:
 	(image.material as ShaderMaterial).set_shader_parameter("width", OUTLINE_WIDTH if value else 0.0)
+	
+func _toggle_colliders(value: bool) -> void:
+	for collider in colliders:
+		collider.disabled = value
+
+func get_picked_up() -> void:
+	toggle_outline(false)
+	_toggle_colliders(true)
+
+func get_dropped() -> void:
+	await get_tree().create_timer(0.3).timeout
+	_toggle_colliders(false)
--- player/maro_3d.gd
+++ player/maro_3d.gd
@@ -3,10 +3,23 @@ class_name Maro3D extends CharacterBody2D
 @export var item: ItemResource
 @onready var interact_area: Area2D = %InteractArea
 
+@onready var sprite: Sprite2D = %Sprite
+@onready var item_position_right: Node2D = %ItemPositionRight
+@onready var item_position_left: Node2D = %ItemPositionLeft
+
 var space_controller: MaroSpaceController = MaroSpaceController.new()
 var tetris_controller: TetrisController = TetrisController.new()
 
 var controller: MaroController = space_controller
+
+var item_to_pick: Item
+var picked_item: Item
+
+var last_dir := Direction.Right
+
+enum Direction {
+	Left, Right
+}
 
 func _ready() -> void:
 	controller = space_controller
@@ -15,15 +28,50 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var movement_input: Vector2 = controller.get_movement_input(delta)
+	
+	if movement_input.x < 0:
+		last_dir = Direction.Left
+	else:
+		last_dir = Direction.Right
+
+	_handle_direction_change()
 	move_and_collide(movement_input)
 
+	if Input.is_action_just_pressed("interact_p1"):
+		if item_to_pick == null and picked_item != null:
+			_drop_item()
+		elif item_to_pick != null and picked_item != null:
+			_drop_item()
+			_pick_item()
+		else:
+			_pick_item()
+
+func _handle_direction_change() -> void:
+	sprite.flip_h = last_dir == Direction.Left
+	if picked_item != null:
+		picked_item.position = item_position_left.position if last_dir == Direction.Left else item_position_right.position
+
+func _pick_item() -> void:
+	picked_item = item_to_pick
+	picked_item.get_picked_up()
+	picked_item.reparent(self)
+	_handle_direction_change()
+	item_to_pick = null
+
+func _drop_item() -> void:
+	picked_item.reparent(get_parent())
+	picked_item.get_dropped()
+	picked_item = null
+
 func _on_interact_area_enter(body: Node2D) -> void:
-	if body is Item:
+	if body is Item and (body as Item) != picked_item:
 		(body as Item).toggle_outline(true)
+		item_to_pick = body
 
 func _on_interact_area_exit(body: Node2D) -> void:
 	if body is Item:
 		(body as Item).toggle_outline(false)
+		item_to_pick = null
 
 func to_tetris(entry_pos: Vector2) -> void:
 	controller = tetris_controller
